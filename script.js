b699dd755343bd9fd5f37af847db29bbfa64ab67
krypto <clayjeansen3@gmail.com>
2026-08-28 00:59:19 +0530

    add constructor fucntion for creating book object

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-console.log("Hello odin!");
-const num = 69;
+const myBooks = [];
+function Book(title, author, pages, status){
+    if (!new.target) {
+        throw Error("You must use the new operator");
+    }
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+    this.id = crypto.randomUUID();
+}
+
+function addBookToLibrary(title, author, pages, status){
+    const book = new Book(title, author, pages, status);
+    myBooks.push(book);
+}
+
+function displayBook(){
+    
+}
